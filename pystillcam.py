9122bdea11900c9dfd97efcfe19e72fc82edc8c6
Wes LaMarche <pi@raspberrypi.(none)>
2015-06-24 20:48:33 -0500

    Test with continuous picture taking

--- pystillcam.py
+++ pystillcam.py
@@ -7,16 +7,29 @@
 import sys
 import time
 import pickle
+import picamera
 
 def main(argv):
 
-   filename = 'settings.txt'
+   s = 'settings.txt'
 
-   f = open(filename, 'r')
+   f = open(s, 'r')
    r = pickle.load(f)
    f.close()
 
    print r
+
+   cam = picamera.PiCamera()
+   cam.resolution = (r['width'], r['height'])
+   cam.saturation = r['style']
+   #cam.led = False
+
+   print('Starting.') 
+   for filename in cam.capture_continuous('pic/img{counter:03d}.jpg'):
+        print('Captured %s' % filename)
+        time.sleep(15) 
+   
+   
     
 if __name__ == "__main__":
     main(sys.argv)
