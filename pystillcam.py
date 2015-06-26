18931a408279984573790fb96748b5a62392158f
Wes LaMarche <pi@raspberrypi.(none)>
2015-06-26 17:03:27 -0500

    Add simple logging

--- pystillcam.py
+++ pystillcam.py
@@ -9,6 +9,9 @@ import time
 import pickle
 import picamera
 
+def log(msg):
+   print time.strftime("%Y-%m-%d %H:%M:%S | ") + msg
+
 def main(argv):
 
    s = 'settings.txt'
@@ -17,28 +20,35 @@ def main(argv):
    r = pickle.load(f)
    f.close()
 
-   print r
+   log('Initialiing.')
+   log(str(r))
 
-   cam = picamera.PiCamera()
-   cam.resolution = (r['width'], r['height'])
-   cam.saturation = r['style']
-   cam.ISO = r['ISO']
-   cam.exposeure_mode = r['mode']
-   cam.exposure_compensation = r['ev']
-   cam.image_effect = r['effect']
-   cam.exif_tags['IFD0.Copyright'] = r['rights']
-   cam.exif_tags['EXIF.UserComment'] = r['comment']
-   #cam.led = False
+   i = 1;
+   while True:
+       cam = picamera.PiCamera()
+       cam.resolution = (r['width'], r['height'])
+       cam.saturation = r['style']
+       cam.ISO = r['ISO']
+       cam.exposeure_mode = r['mode']
+       cam.exposure_compensation = r['ev']
+       cam.image_effect = r['effect']
+       cam.exif_tags['IFD0.Copyright'] = r['rights']
+       cam.exif_tags['EXIF.UserComment'] = r['comment']
+       time.sleep(1) # Camera warm-up time
+       filename = 'pic/img%03d.jpg' % i
+       cam.capture(filename)
+       cam.close()
+       log(str('Captured %s' % filename))
+       i += 1
+       time.sleep(4) # Take approximately a picture per minue
+       
 
-   print('Starting.') 
-   for filename in cam.capture_continuous('pic/img{timestamp:%Y%m%d}{counter:03d}.jpg', format='jpeg'):
+'''   for filename in cam.capture_continuous('pic/img{timestamp:%Y%m%d}{counter:03d}.jpg', format='jpeg'):
        print('Captured %s' % filename)
        print('Sutter speed: %s' % cam.exposure_speed)
        print cam.framerate
        print cam.exif_tags
-       time.sleep(15) 
-   
-   
+       time.sleep(15) '''
     
 if __name__ == "__main__":
     main(sys.argv)
